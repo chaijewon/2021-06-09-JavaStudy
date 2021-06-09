eb06f80f70372605fd3e16ce99a395c3bc74d766
chaijewon <vcandjava@nate.com>
2021-06-09 15:19:53 +0900

    commit

---
+++ FirstProject/src/MainClass.java
@@ -0,0 +1,28 @@
+// 전체 주석 ctrl+shift+ /
+//  주석 풀기 ctrl+shift+ \
+// 1. 출력 => sysout(Ctrl+space)
+// 2. main이 없는 경우 (프로그램의 시작점) main(Ctrl+space) => enter
+
+public class MainClass {
+
+	
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * }
+	 */
+	 
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * // 출력 (주석) System.out.println("Hello Java!!"); // 대소문자 구분
+	 * //System.out.println("Hello Java"); ///System.out.println();
+	 * System.out.println(); // Ctrl+space => sysout
+	 * 
+	 * }
+	 */
+	 
+
+}
---
+++ FirstProject/src/MainClass2.java
@@ -0,0 +1,9 @@
+// 실행은 ctrl+F11
+// 컴퓨터에 명령을 내린다 
+public class MainClass2 {
+	
+  public static void main(String[] args) {
+	  // Hello출력 
+	  System.out.println("Hello");// Hello를 출력 해라
+  }
+}
